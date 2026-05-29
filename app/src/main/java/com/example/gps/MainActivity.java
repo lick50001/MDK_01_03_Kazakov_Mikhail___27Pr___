c044e8eff80f7851115a94bf52645a93ee89e99c
Misha <axxuetam@gmail.com>
2026-05-29 14:23:48 +0500

    onStart onStop Казаков

--- app/src/main/java/com/example/gps/MainActivity.java
+++ app/src/main/java/com/example/gps/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.gps;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.graphics.Point;
 import android.location.Location;
 import android.location.LocationListener;
@@ -11,6 +13,7 @@ import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -49,5 +52,24 @@ public class MainActivity extends AppCompatActivity {
         mapView = findViewById(R.id.mapview);
         textAddress = findViewById(R.id.edittext);
         _LocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        MapKitFactory.getInstance.onStart();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
+        _LocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 10, _locationListioner);
+        _LocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 10, _locationListioner);
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        mapView.onStop();
+        MapKitFactory.getInstance().onStop();
     }
 }
